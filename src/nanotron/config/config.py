7948c5d0c6639d9c277af2451cfb8ffb840899be
Arcana-2236 <zhengyangwang@ucsb.edu>
2025-03-03 16:39:09 -0800

    CoLA Pipeline, Fixed model_config is a dict for custom model config classes

--- src/nanotron/config/config.py
+++ src/nanotron/config/config.py
@@ -227,7 +227,9 @@ class ModelArgs:
         if isinstance(self.dtype, str):
             self.dtype = cast_str_to_torch_dtype(self.dtype)
 
-        self.model_config._is_using_mup = isinstance(self.init_method, SpectralMupInit)
+        # Here model_config is a dict for custom model config classes
+        # Solution 1: comment this line
+        # self.model_config._is_using_mup = isinstance(self.init_method, SpectralMupInit)
 
         # if self.model_config.max_position_embeddings is None:
         #     self.model_config.max_position_embeddings = 0
