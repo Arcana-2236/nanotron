ad13e5765f79b78a8766c774a8114bcdb014d152
Alvin <ziyueliu@ucsb.edu>
2025-12-24 23:17:21 -0800

    NERSC training script tested

---
+++ train_basic_cola_60m_1b_token.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#SBATCH -A m4788_g
+#SBATCH -q premium
+#SBATCH -C gpu&hbm40g
+#SBATCH -t 1:00:00
+#SBATCH --gpus-per-node=4
+#SBATCH --ntasks-per-node=1
+
+cd /global/cfs/cdirs/m4788/alvinliu/repo/nanotron
+module load python/3.10
+module load cudatoolkit/12.4
+source /global/cfs/cdirs/m4788/alvinliu/envs/nanotron/bin/activate
+
+MASTER_PORT=$(( RANDOM + 1000 ))
+MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+WORLD_SIZE=$((SLURM_JOB_NUM_NODES * 4))
+
+TP=${TP:-1}
+PP=${PP:-1}
+
+RUN_NAME=${RUN_NAME:-"None"}
+CONFIG_NAME=${CONFIG_NAME:-"cola_llama_60m"}
+LR=${LR:-"0.004"}
+BZ=${BZ:-"8"}
+TBZ=${TBZ:-"384"}
+CONTINUE=${CONTINUE:-"none"}
+if [ "${CONTINUE}" != "none" ]; then
+    readonly continue_from_flag="--resume_checkpoint_path=$CONTINUE"
+else
+    readonly continue_from_flag=""
+fi
+
+RUN_NAME=$CONFIG_NAME-LR-$LR
+TAG=${TAG:-"none"}
+if [ "${TAG}" != "none" ]; then
+    RUN_NAME=$TAG-$RUN_NAME
+fi
+WU=${WU:-"180"}
+if [ "${WU}" != "180" ]; then
+    RUN_NAME=$RUN_NAME-WU-$WU
+fi
+
+HF_HOME="/global/cfs/cdirs/m4645/alvinliu/workspace/datasets/.cache/huggingface"
+
+HF_HOME=$HF_HOME MASTER_ADDR=$MASTER_ADDR MASTER_PORT=$MASTER_PORT WORLD_SIZE=$WORLD_SIZE \
+    srun -u torchrun --nproc-per-node=4 --master-port=$MASTER_PORT --nnodes=$SLURM_JOB_NUM_NODES \
+    --rdzv-backend=c10d --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT examples/cola/train_basic_cola.py \
+    --config-file examples/cola/config_$CONFIG_NAME.yaml \
+    --hf-dataset-or-datasets /pscratch/sd/a/alvinliu/datasets/seq_len_4096 \
+    --lr $LR \
+    --micro-batch-size $BZ \
+    --batch-accumulation-per-replica $((TBZ / (BZ * WORLD_SIZE))) \
+    --lr-warmup-steps $WU \
+    --tp $TP \
+    --pp $PP \
+    --dp $WORLD_SIZE \
+    $continue_from_flag
